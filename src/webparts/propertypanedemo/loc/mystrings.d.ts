d2abcbf9c11c7b900337f3fe354f6c57839ea791
Mehnaz <csmehnazmalik@gmail.com>
2023-02-09 23:01:53 +0530

    first commit

---
+++ src/webparts/propertypanedemo/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IPropertypanedemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'PropertypanedemoWebPartStrings' {
+  const strings: IPropertypanedemoWebPartStrings;
+  export = strings;
+}
